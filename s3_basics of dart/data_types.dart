fdb5bc6c8ada50ab73fbdde18ac64dbc4a85f246
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-31 13:32:37 +0600

    3.1_overview of data types in dart_with pic

--- s3_basics of dart/data_types.dart
+++ s3_basics of dart/data_types.dart
@@ -8,5 +8,19 @@ var myUpperName = myName.toUpperCase();
 print(myUpperName);
   myName = myName.toUpperCase();
   print(myName);
+  /*
+  Built-in types
+The Dart language has special support for the following:
+    Numbers (int, double)
+    Strings (String)
+    Booleans (bool)
+    Records ((value1, value2))
+    Lists (List, also known as arrays)
+    Sets (Set)
+    Maps (Map)
+    Runes (Runes; often replaced by the characters API)
+    Symbols (Symbol)
+    The value null (Null)
+  */
 
 }
