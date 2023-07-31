036562461ce0dcdd3513afb1993f8ddddef557e6
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-31 13:09:42 +0600

    2_basic math with dart_t4_s3

--- s3_basics of dart/dartFirst.dart
+++ s3_basics of dart/dartFirst.dart
@@ -1,3 +1,19 @@
+import 'dart:math';
 void main(){
-  print ("Hello World !");
+  //print ("Hello World !");
+  //above the code
+  print(1+1);
+  print(10-2);
+  print(3*3);
+  print(4/2);
+  print(10/2);
+  print(10/0);
+  print(0/10);
+  print(pow(2,4));
+  /*
+  this is first line of comment
+  this is second line of comment
+  this is thirt line
+  Programmer Sohan
+   */
 }
