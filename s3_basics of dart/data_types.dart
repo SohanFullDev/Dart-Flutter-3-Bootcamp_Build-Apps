ef3c18f9f4ed43bedd0fc67050dcb376e4a875c8
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-31 13:27:31 +0600

    3_overview of data types in dart_t5_s3

---
+++ s3_basics of dart/data_types.dart
@@ -0,0 +1,12 @@
+void main(){
+  var myName = "Sohan";
+ // print(myName);
+//  print(myName.length);
+print(myName.toUpperCase());
+print(myName);
+var myUpperName = myName.toUpperCase();
+print(myUpperName);
+  myName = myName.toUpperCase();
+  print(myName);
+
+}
