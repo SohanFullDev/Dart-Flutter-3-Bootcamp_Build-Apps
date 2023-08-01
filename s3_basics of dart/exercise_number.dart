4c6ba6ac2a24129784b29e871ec572cd007653c3
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-01 10:57:38 +0600

    5_exercise numbers_t7_s3

---
+++ s3_basics of dart/exercise_number.dart
@@ -0,0 +1,14 @@
+void main(){
+  //given the double below, assign it
+  // to a new variable where its rounded to the
+  // nearest integer
+  var someNum = 3.1;
+    print(someNum.round());
+    var someNum2 = 3.5;
+    print(someNum2.round());
+    print(someNum.ceil());
+    print(someNum.floor());
+
+}
+//BONUS QUESTION: In the cases of 0.5, how can you
+//ensure its rounded up or down?
