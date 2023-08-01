cb495204dde31608de34042ba0acb88ad9207f89
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-01 12:25:40 +0600

    7_exercise strings_t9_s3

---
+++ s3_basics of dart/exercise_strings.dart
@@ -0,0 +1,7 @@
+void main(){
+  //given the string below, replace all the
+  // "x"s with a blank string
+  var errorMessage = "Uxh oxh, there's a bunch of txypos";
+  print(errorMessage.replaceAll('x',''));
+
+}
