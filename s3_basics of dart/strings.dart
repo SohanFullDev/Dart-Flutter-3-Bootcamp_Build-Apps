2bd0f5f999eaba34c2b72b3fe71cd103476af368
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-01 12:01:54 +0600

    6_strings_t8_s3

---
+++ s3_basics of dart/strings.dart
@@ -0,0 +1,12 @@
+void main(){
+  var nameOne = 'Martin';
+  var nameTwo = 'Sohan';
+  print(nameOne);
+  print(nameTwo);
+ // String nameTwo = "Jose";
+  var myNum = 10;
+  print("My number is \$$myNum");
+  print("The length of nameTwo is ${nameTwo.length}");
+  print("The upper case version of NameOne is ${nameOne.toUpperCase()}");
+
+}
