0b72ef67131430414a1a3902a95c40c6a863fc7a
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-01 16:30:52 +0600

    9.1_Lists part1_t11_s3

---
+++ s3_basics of dart/lists.dart
@@ -0,0 +1,11 @@
+void main(){
+
+var myListNum = [10,20,30];
+var myListAlpha = ['A','B','C','D','E'];
+var myListMixed = [10,20,'A'];
+
+print(myListNum);
+print(myListAlpha);
+print(myListMixed);
+
+}
