3c450423c16215832a7f85a60d6b142a8b03adbc
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-05 16:28:31 +0600

    11_sets_s3_t13

---
+++ s3_basics of dart/sets.dart
@@ -0,0 +1,14 @@
+void main(){
+  var elements = {'O','K','Ni','Rd'};
+  print(elements);
+
+  elements.add('F');
+  print(elements);
+
+  elements.addAll(['H','C']);
+  print(elements);
+
+  elements.add('C');
+  print(elements);
+
+}
