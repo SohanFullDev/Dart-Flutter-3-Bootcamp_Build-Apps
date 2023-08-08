f9d8ddcab4607abd64a6deb073f5c91c43c75084
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-08 15:33:34 +0600

    13_Maps_s3_t15

---
+++ s3_basics of dart/maps.dart
@@ -0,0 +1,14 @@
+void main(){
+  var employees = {
+    'David': 10,
+    'Claire':5,
+    'Calvin':3,
+
+  };
+  print(employees);
+  print(employees['david']);
+  print(employees['Jose']);
+  employees['ZED'] = 100;
+  print(employees);
+
+}
