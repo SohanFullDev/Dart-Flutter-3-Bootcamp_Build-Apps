e37bb76ad4b3c7aee4d46fa71c25bf6d937c7181
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-17 10:13:47 +0600

    16.2_comparison-operators_part2_s3t18

--- s3_basics of dart/comparison_operators.dart
+++ s3_basics of dart/comparison_operators.dart
@@ -1,15 +1,13 @@
 void main(){
- // print(3>2);
- var a = 10;
- var b = 100;
- //print(b<a);
- //print(10 <= 10);
- //print(10 >= 10);
- //print(10 == 10);
- var nameOne = "Jose";
- var nameTwo = "Mimi";
- //print(nameOne == nameTwo);
- print(nameOne != nameTwo);
+  /*
+print('BEFORE ASSERT');
+//assert(1==1);
+assert(1 != 1 );
+print('AFTER ASSERT');
+*/
+var a = 50;
+print(a.runtimeType);
 
+print(a is String);
 
 }
